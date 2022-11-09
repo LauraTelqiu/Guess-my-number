546afea94c0ef581b9b5f49398e37ce7d721e946
Laura <lauratelqiu@gmail.com>
2022-11-09 18:28:28 -0500

    game logic

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-'use strict';
+"use strict";
+const secretNumber = Math.floor(Math.random() * 21);
+
+
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
+function eventListener() {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "🛑 No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0
+
+    }
+
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+}
+
+document.querySelector(".check").addEventListener("click", eventListener);
+
+  //score++;
+  //document.querySelector(".score").textContent = score;
